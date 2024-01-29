01d5993aea257788d9c79744547c898674c1ed76
olofvndrhr <ivan@schaller.sh>
2024-01-29 13:37:09 +0100

    update tag script

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -2,6 +2,8 @@
 
 set -xeuo pipefail
 
+current_date="$(date +%Y%m%d)"
+
 push_tag() {
     local tag_name="${1}"
     git tag --force "${tag_name}" -m "${tag_name}"
@@ -10,12 +12,12 @@ push_tag() {
 
 case "${1}" in
     "rec" | "recursor")
-        push_tag "rec-${2}"
+        push_tag "rec-${2}+${current_date}"
         ;;
     "auth" | "authoritative")
-        push_tag "auth-${2}"
+        push_tag "auth-${2}+${current_date}"
         ;;
     "ls" | "lightningstream")
-        push_tag "ls-${2}"
+        push_tag "ls-${2}+${current_date}"
         ;;
 esac
