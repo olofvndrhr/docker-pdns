01e057da3252a02b0390f35411e14de91279e810
Ivan Schaller <ivan@schaller.sh>
2024-07-17 19:53:00 +0200

    update tag script
    Signed-off-by: Ivan Schaller <ivan@schaller.sh>

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -19,21 +19,20 @@ example:
 EOF
 fi
 
-push_tag() {
+set_tag() {
     set -x
     local tag_name="${1}"
     git tag "${tag_name}" -m "${tag_name}"
-    git push origin "${tag_name}"
 }
 
 case "${1}" in
     "rec" | "recursor")
-        push_tag "rec-${2}+${current_date}"
+        set_tag "rec-${2}+${current_date}"
         ;;
     "auth" | "authoritative")
-        push_tag "auth-${2}+${current_date}"
+        set_tag "auth-${2}+${current_date}"
         ;;
     "ls" | "lightningstream")
-        push_tag "ls-${2}+${current_date}"
+        set_tag "ls-${2}+${current_date}"
         ;;
 esac
